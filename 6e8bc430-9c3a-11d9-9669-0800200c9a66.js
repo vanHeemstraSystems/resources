87079ba1fa77811979fe8625950d7bd672f1ce24
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-20 20:37:39 +0100

    Modified

--- 6e8bc430-9c3a-11d9-9669-0800200c9a66.js
+++ 6e8bc430-9c3a-11d9-9669-0800200c9a66.js
@@ -6,7 +6,7 @@ var path = require('../libraries/path');
 var paths = require('../paths/paths');
 // Bluebird has the incredibly useful functionality of enabling you to ‘promisfy’ modules which do not return promises. 
 // For example, to promisfy the fs module, simply require bluebird and a promisified version of fs.
-var Promise = require(path.join(paths.libraries, '/bluebird.js'));
+var Promise = require(path.join(paths.libraries, '/promise.js'));
 //var fs = Promise.promisifyAll(require(path.join(paths.libraries, '/fs.js')));
 
 module.exports = function() {
@@ -14,11 +14,11 @@ module.exports = function() {
   var _Resource = {};
   // Create a new Promise
   return new Promise(function(resolve) {
-	console.log('Resource: 6e8bc430-9c3a-11d9-9669-0800200c9a66 - inside Promise');
+	  console.log('Resource: 6e8bc430-9c3a-11d9-9669-0800200c9a66 - inside Promise');
     _Resource = {
       URI : 'urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66'
     }; 
-	console.log('Resource: 6e8bc430-9c3a-11d9-9669-0800200c9a66 - resolve');
-	resolve(_Resource); 
+	  console.log('Resource: 6e8bc430-9c3a-11d9-9669-0800200c9a66 - resolve');
+	  resolve(_Resource); 
   });
 }
--- resources.js
+++ resources.js
@@ -1,30 +1,29 @@
 /*
  * resources.js
  *
- * Input param: uuid
+ * input: uuid - an Object
  *
- * Returns: a Promise
+ * output: resolve - a Promise
  */
-var path = require('../libraries/path');
-var paths = require('../paths/paths');
-// Bluebird has the incredibly useful functionality of enabling you to ‘promisfy’ modules which do not return promises. 
-// For example, to promisfy the fs module, simply require bluebird and a promisified version of fs.
-var Promise = require(path.join(paths.libraries, '/bluebird.js'));
-//var fs = Promise.promisifyAll(require(path.join(paths.libraries, '/fs.js')));
-
 module.exports = function(uuid) {
   console.log('Resources - called');
-  var _Resources = {};
-  // Create a new Promise
-  return new Promise(function(resolve) {
-	console.log('Resources - inside Promise');
-	var resource = require('./'+uuid+'.js'); // A function that returns a Promise
-	resource()
-      .then(function(resource) {
-        console.log('Resources - resource: ', resource);
-        _Resources.resource = resource;
-      });	
-	console.log('Resources - resolve');	
-	resolve(_Resources);  
-  });
+  var _Me = {};
+  var path = require('../libraries/path'); //TEMP hard coded
+  var paths = require('../paths/paths'); //TEMP hard coded
+  var promise = require(path.join(paths.libraries, '/promise.js')); //TEMP hard coded
+  var _resource = require('./'+uuid+'.js'); // A function that returns a Promise
+  var join = promise.join;
+  return new promise(function(resolve) {
+    join(_resource(), function(resource) {
+      _Me.resource = resource;
+    }); // eof join
+    console.log('resources - resolve(_Me): ', _Me);
+    resolve(_Me);
+  })
+  .catch(function(error) {
+    console.log('resources - error: ', error);
+  })
+  .finally(function() {
+    console.log('resources - finally');
+  }); // eof promise
 }
