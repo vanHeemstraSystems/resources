e4b7a5de85fd114e615e07cd345c7d7ea875c603
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-25 11:25:32 +0100

    Modified

--- resource/6e8bc430-9c3a-11d9-9669-0800200c9a66.js
+++ resource/6e8bc430-9c3a-11d9-9669-0800200c9a66.js
@@ -1,4 +1,4 @@
-function 6e8bc430-9c3a-11d9-9669-0800200c9a66() {
+function _6e8bc430_9c3a_11d9_9669_0800200c9a66() {
   var _Me = {};
   _Me.URI = 'urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66';
   return (_Me);
--- resource/index.js
+++ resource/index.js
@@ -1,7 +1,7 @@
 /*
  * index.js
  */
-var Resource6e8bc430-9c3a-11d9-9669-0800200c9a66 = require(__dirname+'/6e8bc430-9c3a-11d9-9669-0800200c9a66.js');
+var Resource6e8bc430_9c3a_11d9_9669_0800200c9a66 = require(__dirname+'/6e8bc430-9c3a-11d9-9669-0800200c9a66.js');
 
 /**
  * Create a new Utility that let users create sub-utilities.
@@ -10,11 +10,11 @@ var Resource6e8bc430-9c3a-11d9-9669-0800200c9a66 = require(__dirname+'/6e8bc430-
 function Resource() { }
 
 /**
- * Create a new Resource6e8bc430-9c3a-11d9-9669-0800200c9a66 object.
- * @return {Resource6e8bc430-9c3a-11d9-9669-0800200c9a66}
+ * Create a new Resource6e8bc430_9c3a_11d9_9669_0800200c9a66 object.
+ * @return {Resource6e8bc430_9c3a_11d9_9669_0800200c9a66}
  */
-Resource.prototype.6e8bc430-9c3a-11d9-9669-0800200c9a66 = function() {
-  return new Resource6e8bc430-9c3a-11d9-9669-0800200c9a66();
+Resource.prototype._6e8bc430_9c3a_11d9_9669_0800200c9a66 = function() {
+  return new Resource6e8bc430_9c3a_11d9_9669_0800200c9a66();
 }
 
 module.exports = new Resource();
