ba2fabf60bb937089747cfa34afb64e7a41e789a
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-25 10:57:12 +0100

    Modified

--- resources.js
+++ resources.js
@@ -1,29 +1,21 @@
 /*
  * resources.js
- *
- * input: input - an Object
- *
- * output: resolve - a Promise
  */
-module.exports = function() {
-  console.log('resources - called');
-  var _Me = {};
-  var path = require('../libraries/path');
-  var paths = require('../paths/paths'); 
-  var promise = require(path.join(paths.libraries, '/promise.js'));
-  var _resource = require(__dirname+'/resource.js');
-  var join = promise.join;
-  return new promise(function(resolve) {
-    join(_resource(), function(resource) {
-      _Me.resource = resource;
-    }); // eof join
-    console.log('resources - resolve(_Me): ', _Me);
-    resolve(_Me);
-  }) // eof promise
-  .catch(function(error) {
-    console.log('resources - error: ', error);
-  }) // eof catch
-  .finally(function() {
-    console.log('resources - finally');
-  }); // eof finally
-} // eof module
+var ResourcesResource = require(__dirname+'/resource.js');
+
+/**
+ * Create a new Resources that let users create sub-resources.
+ * @return {Resources}
+ */
+function Resources() { }
+
+/**
+ * Create a new ResourcesResource object.
+ * @return {ResourcesResource}
+ */
+Resources.prototype.resource = function() {
+  return new ResourcesResource();
+}
+
+//ORIGINAL module.exports = new Resources();
+module.exports = function() { return new Resources(); }
