b63f81f2c967db873fdfa436425017e26cae9ba6
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-25 15:18:34 +0100

    Modified

--- resource/6e8bc430-9c3a-11d9-9669-0800200c9a66.js
+++ resource/6e8bc430-9c3a-11d9-9669-0800200c9a66.js
@@ -1,5 +1,5 @@
 function _6e8bc430_9c3a_11d9_9669_0800200c9a66() {
-  var _Me = {};
-  _Me.URI = 'urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66';
-  return (_Me);
+  this.URI = 'urn:uuid:6e8bc430-9c3a-11d9-9669-0800200c9a66';
 }
+
+module.exports = function() { return new _6e8bc430_9c3a_11d9_9669_0800200c9a66(); }
